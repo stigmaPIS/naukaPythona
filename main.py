71f3cb5adb1ee08e8e000502ae89fa515f8c7b39
Antoni <stigmapis@gmail.com>
2023-11-24 10:28:21 +0100

    Nowy projekt

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import random
+iloscoczek = " "
+def rzut(x):
+    punkty = 100
+    for i in range(x):
+        iloscoczek = random.randint(1, 6)
+        if iloscoczek == 1:
+            punkty = 1
+            print("wypadło:", iloscoczek)
+            print("punkty:", punkty,'\n')
+        if iloscoczek == 2 or iloscoczek == 3:
+            punkty = punkty / 2
+            print("wypadło:", iloscoczek)
+            print("punkty:", punkty,'\n')
+        if iloscoczek == 4 or iloscoczek == 5:
+            punkty = punkty * 2
+            print("wypadło:", iloscoczek)
+            print("punkty:", punkty,'\n')
+        if iloscoczek == 100:
+            punkty = punkty
+            print("wypadło:", iloscoczek)
+            print("punkty:", punkty,'\n')
+rzut(3)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
